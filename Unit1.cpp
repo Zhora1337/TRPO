534adb25072bd1bac8a53ab3e234f105941c87e1
Zhora1337 <zhora1337@protonmail.com>
2019-12-16 10:59:02 +0300

    Recolored

--- Unit1.cpp
+++ Unit1.cpp
@@ -16,9 +16,9 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 //---------------------------------------------------------------------------
 void __fastcall TForm1::Button1Click(TObject *Sender)
 {
-	Form1->Canvas->Brush->Color = clMaroon;
+	Form1->Canvas->Brush->Color = clBlue;
 	Form1->Canvas->Pie(0,0, 20,20, 20,0, 0,0);
-	Form1->Canvas->Brush->Color = clMaroon;
+	Form1->Canvas->Brush->Color = clBlue;
 	Form1->Canvas->Rectangle(20, 20, 100, 100);
 }
 //---------------------------------------------------------------------------
